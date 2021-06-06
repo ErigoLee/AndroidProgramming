e946aca82d5edb6da21ec18132aef83d34b8e409
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-06-06 20:38:01 +0900

    Project3

---
+++ app/src/main/java/kr/ac/duksung/converter2/MainActivity.java
@@ -0,0 +1,68 @@
+package kr.ac.duksung.converter2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edit1, edit2;
+    Button btnAdd, btnSub;
+    TextView textResult;
+    String num1,num2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("초간단 계산기");
+
+        edit1 = (EditText) findViewById(R.id.Edit1);
+        edit2 = (EditText) findViewById(R.id.Edit2);
+        btnAdd = (Button) findViewById(R.id.BtnAdd);
+        btnSub = (Button) findViewById(R.id.BtnSub);
+        textResult = (TextView) findViewById(R.id.TextResult);
+
+        btnAdd.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                num1 = edit1.getText().toString();
+                num1 = num1.trim();
+                num2 = edit2.getText().toString();
+                num2 = num2.trim();
+                if(num1.equals(""))
+                    textResult.setText("숫자1을 입력하세요");
+                else if(num2.equals(""))
+                    textResult.setText("숫자2를 입력하세요.");
+                else{
+                    Integer result = Integer.parseInt(num1)+Integer.parseInt(num2);
+                    textResult.setText("계산 결과 : "+result.toString());
+                }
+            }
+
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                num1 = edit1.getText().toString();
+                num1 = num1.trim();
+                num2 = edit2.getText().toString();
+                num2 = num2.trim();
+                if(num1.equals(""))
+                    textResult.setText("숫자1을 입력하세요");
+                else if(num2.equals(""))
+                    textResult.setText("숫자2를 입력하세요.");
+                else{
+                    Integer result = Integer.parseInt(num1)-Integer.parseInt(num2);
+                    textResult.setText("계산 결과 : "+result.toString());
+                }
+            }
+        });
+    }
+}
